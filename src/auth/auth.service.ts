a4691fddb77c9041f473209bab4bffc6ee3a9c67
minhvuong123 <minhvuong12222@gmail.com>
2023-10-16 20:22:19 +0700

    Task management project

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
+import { AuthCredentialsDto } from './dto/auth-credentisl.dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersReppository: UsersRepository,
+    private jwtService: JwtService,
+  ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.usersReppository.createUser(authCredentialsDto);
+  }
+
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    const { username, password } = authCredentialsDto;
+
+    const user = await this.usersReppository.findOne({ where: { username } });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload: JwtPayload = { username };
+      const accessToken: string = await this.jwtService.sign(payload);
+
+      return {
+        accessToken,
+      };
+    } else {
+      throw new UnauthorizedException('Plese chck your login credentials');
+    }
+  }
+}
